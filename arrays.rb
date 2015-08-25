9e6d3ff12e166e5e1d0c892bc0cbd0b8033946a7
Heather Sockney <shockney@atlanticbb.net>
2015-08-25 14:45:55 -0400

    Update arrays.rb

--- arrays.rb
+++ arrays.rb
@@ -1,9 +1,15 @@
 list = 1
 numbers = []
+
 while list <= 100 
-  numbers.push(list)
+ 
+  if list % 3 == 0
+   numbers.push"Greene"
+   else
+   numbers.push(list)
+    
+   end
   list += 1
   
  end
- numbers[2]="Greene"
 
