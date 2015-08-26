1085390922578459523e4903e54b6b112f26af06
Heather Sockney <shockney@atlanticbb.net>
2015-08-26 10:24:31 -0400

    Update arrays.rb

--- arrays.rb
+++ arrays.rb
@@ -1,17 +1,21 @@
-list = 1
-numbers = []
+list=1
+numbers=[]
 
-while list <= 100 
-   if list % 3==0 && list % 5==0
-     numbers.push"greene_county"
-   elsif list % 3 == 0
-     numbers.push"Greene"
-   elsif list % 5 == 0
-     numbers.push"County"
-   else
-     numbers.push(list)
-    
-   end
-  list += 1
-  
+
+while list <= 100
+ if list % 3 == 0 && list % 5 == 0
+    numbers.push( "greene_county" )
+ elsif list % 3 == 0            
+    numbers.push( "greene" )            
+  elsif list % 5 == 0         
+    numbers.push( "county" )
+    else numbers.push(list)
  end
+ list += 1
+ 
+end
+
+puts"What index position 1-100 would you like to see the value for(index positions start at 0 for 1, 1 for 2,etc)?" 
+position=gets.chomp.to_i
+print"You have chosen #{position}"
+puts " The value is #{numbers.fetch(position)}"
